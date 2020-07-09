f470da6db0f80b05f6bfce855470e04d44e5b113
huxin <xin.hu@servingcloud.com>
2020-07-09 15:16:15 +0800

    注释 掉部分的代码

--- testing/conftest.py
+++ testing/conftest.py
@@ -2,15 +2,15 @@
 
 import pytest
 
-def pytest_collection_modifyitems(session, config, items: list):
-    for item in items:
-        if "add" in item.nodeid:
-            item.add_marker(pytest.mark.add)
-        elif "sub" in item.nodeid:
-            item.add_marker(pytest.mark.sub)
-        elif "take" in item.nodeid:
-            item.add_marker(pytest.mark.take)
-        elif "div" in item.nodeid:
-            item.add_marker(pytest.mark.div)
+# def pytest_collection_modifyitems(session, config, items: list):
+#     for item in items:
+#         if "add" in item.nodeid:
+#             item.add_marker(pytest.mark.add)
+#         elif "sub" in item.nodeid:
+#             item.add_marker(pytest.mark.sub)
+#         elif "take" in item.nodeid:
+#             item.add_marker(pytest.mark.take)
+#         elif "div" in item.nodeid:
+#             item.add_marker(pytest.mark.div)
 
     # items.reverse()   # 反向执行 在 testing 目录下执行 pytest -vs
